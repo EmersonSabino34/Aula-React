4e262d41c76acd75e7f1fca283a49b3417432cd9
EmersonSabino34 <168974048+EmersonSabino34@users.noreply.github.com>
2024-10-16 07:04:35 -0300

    Create Exercicio05.jsx

---
+++ src/views/Exercicio05.jsx
@@ -0,0 +1,23 @@
+export function Exercicio04(){
+    return(
+        <div>
+            <h1>Exercicio 04</h1>
+        </div>
+    )
+}
+
+function Exercicio05(){
+    return(
+        <div>
+            <h1>Exercicio 05</h1>
+        </div>
+    )
+}
+
+function Exercicio06(){
+    return(
+        <div>
+            <h1>Exercicio 06</h1>
+        </div>
+    )
+}
